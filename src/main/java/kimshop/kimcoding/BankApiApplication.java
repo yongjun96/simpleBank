47d7025885982c9a79cf19bfa8afa89648159e65
yongjun96 <practice960426@gmail.com>
2024-01-21 00:26:39 +0900

    application.yml , dev, prod, test로 구분해서 나누어 세팅하고 gradle세팅

---
+++ src/main/java/kimshop/kimcoding/BankApiApplication.java
@@ -0,0 +1,13 @@
+package kimshop.kimcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApiApplication.class, args);
+    }
+
+}
